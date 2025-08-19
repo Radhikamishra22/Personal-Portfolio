76bc7073d893fe026e926de3e91ec7cdbbcbb168
Radhika Mishra <misradhika15@gmail.com>
2025-08-20 00:39:37 +0530

    first commit

---
+++ data/user-data.js
@@ -0,0 +1,10 @@
+export const userData = {
+  githubUser: 'Radhikamishra22',
+  github: 'https://github.com/Radhikamishra22',
+  linkedIn: 'https://www.linkedin.com/in/radhika-mishra-286010301/',
+  stackOverflow: 'https://stackoverflow.com/users/31088289/radhika-mishra',
+  leetcode: "https://leetcode.com/Radhikamishra22/",
+  resume: "/C:/Users/RADHIKA%20MISHRA/OneDrive/Documents/Radhikaa22.pdf",
+  skills: ['React', 'NextJS', 'HTML', 'CSS' ,'Javascript', 'NestJS', 'MySql', 'MongoDB'],
+  timezone: '+6'
+};
